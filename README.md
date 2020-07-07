80add06279ec5e4366e56a6bd63acc94d3875d6e
jaden-song <seoA2016##>
2020-07-07 16:30:08 +0900

    session 6 done + code challenges

--- README.md
+++ README.md
@@ -126,3 +126,12 @@ Learning React and ES6 by building a Movie Discovery App. (normadcoders)
 
 ## 6.0 Presenter Structure
 
+
+
+## Code Challenges
+
+- [ ] IMDB Link
+- [ ] Tabs inside of Movie / Show Details (YT Videos, Production Company & Countries)
+- [ ] Collections Link
+- [ ] /collections Route
+- [ ] On TV Show, show seasons and creators
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,11 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Loader from "Components/Loader";
 import Section from "Components/Section";
+import Message from "Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 20px;
 `;
 
 const Form = styled.form`
@@ -29,6 +32,7 @@ const SearchPresenter = ({
   updateTerm
 }) => 
   <Container>
+    <Helmet><title>Search | Mflix</title></Helmet>
     <Form onSubmit={handleSubmit}>
       <Input placeholder="Search Movies or TV Shows..." 
         values={searchTerm} 
@@ -38,14 +42,35 @@ const SearchPresenter = ({
       <>
         {movieResults && movieResults.length > 0 &&
           <Section title="Movie Results">
-            {movieResults.map(movie => <span key={movie.id}>{movie.title}</span>)}
+            {movieResults.map(movie => 
+              <Poster 
+                key={movie.id}
+                id={movie.id}
+                title={movie.original_title}
+                imageUrl={movie.poster_path}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true} />  
+            )}
           </Section>
         }
         {tvResults && tvResults.length > 0 &&
           <Section title="TV Show Results">
-            {tvResults.map(show => <span key={show.id}>{show.name}</span>)}
+            {tvResults.map(show => 
+              <Poster 
+                key={show.id}
+                id={show.id}
+                title={show.original_name}
+                imageUrl={show.poster_path}
+                rating={show.vote_average}
+                year={show.first_air_date && show.first_air_date.substring(0, 4)}
+                isMovie={false} />
+            )}
           </Section>
         }
+        {error && <Message color="#e74c3c" text={error}/>}
+        {tvResults && movieResults && tvResults.length ===0 && movieResults.length ===0
+          && <Message text="Nothing found" color="#95a5a6"/>}
       </>
     }
   </Container>
